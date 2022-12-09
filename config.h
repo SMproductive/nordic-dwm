4bf7d9059e13f1d34e5a95561efe29c15736fe7c
Maximilian Strele <SMproductive@tutanota.de>
2022-12-09 07:54:48 -0500

    st scrolling

--- config.h
+++ config.h
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 /* COMMANDS */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "scroll" };
 static const char mute[] = "amixer set Master toggle";
 static const char louder[] = "amixer set Master 5%+";
 static const char quieter[] = "amixer set Master 5%-";
